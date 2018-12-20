3d26048517d11ae1e7dc64107e71d6bc527170f1
SANVITI <xiongshuangel@163.com>
2018-12-20 15:07:48 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -13,10 +13,10 @@ return array(
 
         /* 测试服务器 */
         'DB_TYPE'   => 'mysql', // 数据库类型
-        'DB_HOST'   => '47.74.254.250', // 服务器地址
-        'DB_NAME'   => 'liantong_yohomm', // 数据库名
-        'DB_USER'   => 'liantong_yohomm', // 用户名
-        'DB_PWD'    => 'JrYxwwSB6mejR5tZ',  // 密码
+        'DB_HOST'   => '', // 服务器地址
+        'DB_NAME'   => '', // 数据库名
+        'DB_USER'   => '', // 用户名
+        'DB_PWD'    => '',  // 密码
         'DB_PORT'   => '3306', // 端口
         'DB_PREFIX' => 'lt_', // 数据库表前缀
 
